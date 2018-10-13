18e8b6d75b8102161aec5d1380a207caea4674c7
sinjs89 <sinjs89@naver.com>
2018-10-13 12:44:36 +0900

    Merge branch 'master' of C:\Users\정섭\git\python\python_selinium with conflicts.

--- com/01_login_bank.py
+++ com/01_login_bank.py
@@ -1,34 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
 
-import time
-
-
-driver = webdriver.Chrome("../chrome/mac/chromedriver")
-driver.implicitly_wait(30)
+driver = webdriver.Chrome("../chrome/window/chromedriver.exe")
 
 driver.get("http://dis.kofia.or.kr/websquare/index.jsp?w2xPath=/wq/fundann/DISMainFlucFund.xml&divisionId=MDIS01006002000000&serviceId=SDIS01006002000")
+driver.implicitly_wait(10)
 
-title = driver.title
-print(title)
+table = driver.title
+print(table)
+driver.implicitly_wait(50)
 
 select = Select(driver.find_element_by_id('fundTyp_input_0'))
 
 print(select)
 
 # select by visible text
-select.select_by_visible_text('혼합주식형')
+select.select_by_visible_text('주식형')
 #
 # # select by value
 # select.select_by_value('1')
 
-
 button = driver.find_element_by_css_selector("#btnSearImg")
 # print(input)
 button.click()
-
-time.sleep(10)
-table = driver.find_element_by_css_selector("#grdMain")
-print(table.text)
-
+driver.implicitly_wait(50)
+table = driver.find_element_by_class_name("grdMain_body_tbody")
+print(table)
